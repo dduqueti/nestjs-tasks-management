b48e21044f902e218248dca011e4b9e6b0547600
Daniel Duque <danieljduque@gmail.com>
2020-07-18 17:55:39 -0500

    Add bcrypt encryption to user

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -6,6 +6,7 @@ import {
   InternalServerErrorException,
   ConflictException,
 } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
@@ -14,7 +15,8 @@ export class UserRepository extends Repository<User> {
 
     const user = new User();
     user.username = username;
-    user.password = password;
+    user.salt = await bcrypt.genSalt();
+    user.password = await this.hashPassword(password, user.salt);
 
     try {
       await user.save();
@@ -26,5 +28,9 @@ export class UserRepository extends Repository<User> {
         throw new InternalServerErrorException();
       }
     }
+  }
+
+  private async hashPassword(password: string, salt: string): Promise<string> {
+    return bcrypt.hash(password, salt);
   }
 }
