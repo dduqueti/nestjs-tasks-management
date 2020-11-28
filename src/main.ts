c8df76594d48178ffa5d59cdd3a3cc856157aa33
Daniel Duque <danieljduque@gmail.com>
2020-11-28 16:04:31 -0500

    Add CORS and allow only for dev env

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,17 @@ import * as config from 'config';
 
 async function bootstrap() {
   // NODE_ENV defines environment
+
   const serverConfig = config.get('server');
   const logger = new Logger('bootstrap');
   const port = process.env.PORT || serverConfig.port;
   const app = await NestFactory.create(AppModule);
+
+  // Only allow in development mode for now
+  if (process.env.NODE_ENV === 'development') {
+    app.enableCors();
+  }
+
   await app.listen(3000);
 
   logger.log(`Application listening on port ${port}`);
